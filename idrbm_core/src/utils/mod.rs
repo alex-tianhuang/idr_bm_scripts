53384ec209bd3aabc72d201f55990906d0177eb8
alex-dsci <alex-dsci@github.com>
2026-03-18 15:24:39 -0400

    add some file readers from llphyscore project

---
+++ idrbm_core/src/utils/mod.rs
@@ -0,0 +1,16 @@
+mod read_file;
+mod read_fasta;
+pub use read_file::read_file;
+pub use read_fasta::read_fasta;
+
+/// Leak a [`bumpalo::collections::Vec`] managed by an arena into a slice
+/// that lives for as long as the arena does not reset.
+/// 
+/// Analogue of [`std::vec::Vec::leak`].
+pub fn leak_vec<'a, T>(buf: bumpalo::collections::Vec<'a, T>) -> &'a mut [T] {
+    let mut buf = std::mem::ManuallyDrop::new(buf);
+    // SAFETY: the arena must de-allocate before this vec does.
+    //         Also, do to the use of `ManuallyDrop`, these bytes
+    //         are not de-allocated and reused by the arena.
+    unsafe { std::mem::transmute::<&mut [T], &'a mut [T]>(buf.as_mut_slice()) }
+}
