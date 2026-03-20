e51806d9615edccdb8bc5a8a24943ac4204ec631
alex-dsci <alex-dsci@github.com>
2026-03-20 00:28:07 -0400

    Add leak_str

--- idrbm_core/src/utils/mod.rs
+++ idrbm_core/src/utils/mod.rs
@@ -14,3 +14,15 @@ pub fn leak_vec<'a, T>(buf: bumpalo::collections::Vec<'a, T>) -> &'a mut [T] {
     //         are not de-allocated and reused by the arena.
     unsafe { std::mem::transmute::<&mut [T], &'a mut [T]>(buf.as_mut_slice()) }
 }
+
+/// Leak a [`bumpalo::collections::String`] managed by an arena into a `str`
+/// that lives for as long as the arena does not reset.
+/// 
+/// Analogue of [`std::string::String::leak`].
+pub fn leak_str<'a>(s: bumpalo::collections::String<'a>) -> &'a mut str {
+    let mut s = std::mem::ManuallyDrop::new(s);
+    // SAFETY: the arena must de-allocate before this vec does.
+    //         Also, do to the use of `ManuallyDrop`, these bytes
+    //         are not de-allocated and reused by the arena.
+    unsafe { std::mem::transmute::<&mut str, &'a mut str>(s.as_mut_str()) }
+}
