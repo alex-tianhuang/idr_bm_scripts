cafa88fe96a5c6b007d1d7e837b46d1d81302713
alex-dsci <alex-dsci@github.com>
2026-03-22 20:17:09 -0400

    bug fix clear buffer before writing

--- idrbm_core/src/utils/compound_header.rs
+++ idrbm_core/src/utils/compound_header.rs
@@ -4,7 +4,7 @@
 /// The delimiter I use.
 const DELIMITER: u8 = b'|';
 /// Takes `N` IDs and joins them with '|' using `csv::Writer`.
-/// 
+///
 /// Make one with [`CompoundHeaderWriter::new`].
 pub struct CompoundHeaderWriter<const N: usize> {
     buf: Vec<u8>,
@@ -16,8 +16,10 @@ impl<const N: usize> CompoundHeaderWriter<N> {
     /// Get a header consisting of `parts` joined by the '|' character.
     pub fn construct_header(&mut self, parts: [&str; N]) -> &str {
         const EXPECT_MSG: &str = "failed to write to vec for some reason";
-        let wtr = std::mem::take(&mut self.buf);
-        let mut writer = csv::WriterBuilder::new().delimiter(DELIMITER).from_writer(wtr);
+        self.buf.clear();
+        let mut writer = csv::WriterBuilder::new()
+            .delimiter(DELIMITER)
+            .from_writer(std::mem::take(&mut self.buf));
         writer.write_record(parts).expect(EXPECT_MSG);
         self.buf = writer.into_inner().expect(EXPECT_MSG);
         str::from_utf8(&self.buf).expect("unexpected non-UTF8 made by joining UTF-8 parts")
