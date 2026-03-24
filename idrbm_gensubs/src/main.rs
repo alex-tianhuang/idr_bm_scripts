a5d269f33902e971a2b7b15a702354643695f385
alex-dsci <alex-dsci@github.com>
2026-03-24 17:45:20 -0400

    update all to use csv module utils

--- idrbm_gensubs/src/main.rs
+++ idrbm_gensubs/src/main.rs
@@ -1,8 +1,16 @@
-use std::{fs::File, io::{BufWriter, Write}, path::PathBuf};
 use anyhow::Error;
 use bumpalo::{Bump, boxed::Box};
 use clap::Parser;
-use idrbm_core::{box_dyn_in, utils::{CompoundHeaderWriter, read_fasta_to_map, read_regions, read_variants}};
+use idrbm_core::{
+    box_dyn_in,
+    csv::{CompoundHeaderWriter, DuplicateRule, read_regions, read_variants},
+    utils::read_fasta_to_map,
+};
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::PathBuf,
+};
 /// Program for substituting variants into wild-type sequences.
 #[derive(Parser)]
 #[command(verbatim_doc_comment)]
@@ -34,30 +42,64 @@ fn main() -> anyhow::Result<()> {
     let arena = Bump::new();
     let mut err_out = alloc_error_handler(log_errs_to, &arena)?;
     let sequences = read_fasta_to_map(&sequences, &arena, &mut *err_out)?;
-    let regions = read_regions(&regions, &arena)?;
-    let variants = read_variants(&variants, &arena)?;
+    let regions = read_regions(&regions, DuplicateRule::LastWins, &arena)?;
+    let variants = read_variants(&variants, DuplicateRule::LastWins, &arena)?;
 
     let file = File::create(&output_file)?;
     let mut wtr = BufWriter::new(file);
     let mut header_writer = CompoundHeaderWriter::new();
     for (protein_id, protein_group) in variants {
-        let whole_sequence = *sequences.get(protein_id).ok_or_else(|| Error::msg(format!("could not find whole protein sequence for protein {}", protein_id)))?;
-        let regions = regions.get(protein_id).ok_or_else(|| Error::msg(format!("could not find region bounds for protein {}", protein_id)))?;
+        let whole_sequence = *sequences.get(protein_id).ok_or_else(|| {
+            Error::msg(format!(
+                "could not find whole protein sequence for protein {}",
+                protein_id
+            ))
+        })?;
+        let regions = regions.get(protein_id).ok_or_else(|| {
+            Error::msg(format!(
+                "could not find region bounds for protein {}",
+                protein_id
+            ))
+        })?;
         for &(region_id, region_group) in protein_group {
-            let (_, region) = regions.iter().find(|rgn| rgn.0 == region_id).ok_or_else(|| Error::msg(format!("could not find region bounds for protein {}, region {}", protein_id, region_id)))?;
+            let (_, region) = regions
+                .iter()
+                .find(|rgn| rgn.0 == region_id)
+                .ok_or_else(|| {
+                    Error::msg(format!(
+                        "could not find region bounds for protein {}, region {}",
+                        protein_id, region_id
+                    ))
+                })?;
             if !region.is_in_bounds_of(whole_sequence) {
-                writeln!(err_out, "region is out of bounds for protein {} (start={}, stop={}, seq.len()={}): {}", region.start(), region.stop(), whole_sequence.len(), protein_id, region_id)?;
+                writeln!(
+                    err_out,
+                    "region is out of bounds for protein {} (start={}, stop={}, seq.len()={}): {}",
+                    region.start(),
+                    region.stop(),
+                    whole_sequence.len(),
+                    protein_id,
+                    region_id
+                )?;
                 continue;
             }
             let prefix = whole_sequence[..region.start()].as_str();
             let suffix = whole_sequence[region.stop()..].as_str();
             for &(variant_id, variant_sequence) in region_group {
-                let compound_header = header_writer.construct_header([protein_id, region_id, variant_id]);
-                write!(wtr, ">{}\n{}{}{}\n", compound_header, prefix, variant_sequence.as_str(), suffix)?;
+                let compound_header =
+                    header_writer.construct_header([protein_id, region_id, variant_id]);
+                write!(
+                    wtr,
+                    ">{}\n{}{}{}\n",
+                    compound_header,
+                    prefix,
+                    variant_sequence.as_str(),
+                    suffix
+                )?;
             }
         }
     }
-    
+
     Ok(())
 }
 
